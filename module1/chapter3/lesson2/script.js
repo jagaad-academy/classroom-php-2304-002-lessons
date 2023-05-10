ac82cb91b160f235e017d69b15d66598a5bcea19
Hennadii Shvedko <hennadii.shvedko@jagaad.com>
2023-05-10 18:02:07 +0200

    M1C3L2 examples

---
+++ module1/chapter3/lesson2/script.js
@@ -0,0 +1,15 @@
+// Finding HTML elements by id
+const elementById = document.getElementById("formTest");
+
+// Finding HTML elements by tag name
+const elementByTagName = document.getElementsByTagName("form");
+
+//Finding HTML elements by class name
+const elementByClassName = document.getElementsByClassName("form-test");
+
+//Finding HTML elements by CSS selectors
+const elementByCssSelector = document.querySelector('.form-elements');
+const elementByCssSelectorAll = document.querySelectorAll('.form-elements');
+
+//Finding HTML elements by HTML object collections
+const form = document.forms["formTest"];
