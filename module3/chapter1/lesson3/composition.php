45e1263b340a1d8e3bbc1bad4c614a5eb0c7fe08
Hennadii Shvedko <hennadii.shvedko@jagaad.com>
2023-07-07 16:03:40 +0200

    M3C1L3

---
+++ module3/chapter1/lesson3/composition.php
@@ -0,0 +1,39 @@
+<?php
+
+class Logger
+{
+    public function log($message)
+    {
+        echo $message . PHP_EOL;
+    }
+
+    public function error($message)
+    {
+        error_log($message);
+        die;
+    }
+}
+
+class DataMigrator
+{
+    private $logger1;
+
+    public function __construct()
+    {
+        $this->logger1 = new Logger();
+    }
+
+    public function migrate()
+    {
+        $this->logger1->log('Migrating. Please wait...');
+    }
+
+    public function showErrorMessage()
+    {
+        $this->logger1->error('Fatal Error!');
+    }
+}
+
+$dataMigrator = new DataMigrator();
+$dataMigrator->migrate();
+$dataMigrator->showErrorMessage();
