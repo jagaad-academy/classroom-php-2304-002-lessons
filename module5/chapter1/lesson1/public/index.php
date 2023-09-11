e6910e7b0be90c03c3df7f2f11699339b70edcc1
Hennadii Shvedko <hennadii.shvedko@jagaad.com>
2023-09-11 18:13:52 +0200

    M5C1L1

---
+++ module5/chapter1/lesson1/public/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * index.php
+ * hennadii.shvedko
+ * 11.09.2023
+ */
+
+
+$servername = "mariadb";
+$username = "test";
+$password = "123";
+$dbname = "test";
+$port = "3306";
+try {
+    new PDO("mysql:host=$servername;port=$port;dbname=$dbname",$username,$password);
+    echo "Connected!";
+} catch (PDOException $exception){
+    echo "Connection failed!";
+    echo $exception->getMessage();
+}
+
+die;
